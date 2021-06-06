dad78f4a9e1550bc28c7c266c466037813bb3f04
merrazurizing <merrazuriz.ing@gmail.com>
2021-06-06 19:38:06 -0400

    Uso de props y faker.js para datos falsos

--- 4.-Props/functional-componenets/src/App.js
+++ 4.-Props/functional-componenets/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import './App.css';
 import Card from './Card';
-
+import faker from 'faker'
 function App() {
   return (
     <div className="App"> 
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card
+        avatar={faker.image.avatar()}
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
+        title={faker.name.jobTitle()}
+      />
+
+      <Card
+        avatar={faker.image.avatar()}
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
+        title={faker.name.jobTitle()}
+      />
+
+      <Card
+        avatar={faker.image.avatar()}
+        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
+        title={faker.name.jobTitle()}
+      />
+
     </div>
   );
 }
--- 4.-Props/functional-componenets/src/Card.js
+++ 4.-Props/functional-componenets/src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css';
 
-export const Card = () => {
+export const Card = (props) => {
+    console.log(props)
     return (    
             <div className="card">
-                <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{width: '100%'}}></img>
+                <img src={props.avatar} alt="Avatar" style={{width: '100%'}}></img>
                 <div className="container">
-                <h4><b>John Doe</b></h4> 
-                <p>Architect & Engineer</p> 
+                <h4><b>{props.name}</b></h4> 
+                <p>{props.title}</p> 
                 </div>
             </div>
     )
