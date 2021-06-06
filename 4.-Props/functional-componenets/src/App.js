67f5c4eed6e2a06cc6613ee651ed761313b043c5
merrazurizing <merrazuriz.ing@gmail.com>
2021-06-06 19:47:47 -0400

    Props children , y variable con div

--- 4.-Props/functional-componenets/src/App.js
+++ 4.-Props/functional-componenets/src/App.js
@@ -3,25 +3,40 @@ import './App.css';
 import Card from './Card';
 import faker from 'faker'
 function App() {
+  const buttonMarkup = (
+    <div>
+      <button className="button button2">SI</button>
+      <button className="button button3">NO</button>
+    </div> 
+  )
+
+
+
   return (
     <div className="App"> 
       <Card
         avatar={faker.image.avatar()}
         name={`${faker.name.firstName()} ${faker.name.firstName()}`}
         title={faker.name.jobTitle()}
-      />
+      >
+        {buttonMarkup}
+      </Card>
 
       <Card
         avatar={faker.image.avatar()}
         name={`${faker.name.firstName()} ${faker.name.firstName()}`}
         title={faker.name.jobTitle()}
-      />
+      >
+        {buttonMarkup}
+      </Card>
 
       <Card
         avatar={faker.image.avatar()}
         name={`${faker.name.firstName()} ${faker.name.firstName()}`}
         title={faker.name.jobTitle()}
-      />
+      >
+        {buttonMarkup}
+      </Card>
 
     </div>
   );
--- 4.-Props/functional-componenets/src/Card.js
+++ 4.-Props/functional-componenets/src/Card.js
@@ -8,7 +8,8 @@ export const Card = (props) => {
                 <img src={props.avatar} alt="Avatar" style={{width: '100%'}}></img>
                 <div className="container">
                 <h4><b>{props.name}</b></h4> 
-                <p>{props.title}</p> 
+                <p>{props.title}</p>
+                <p>{props.children}</p>
                 </div>
             </div>
     )
